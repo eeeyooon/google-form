f71646ddd4c76fb5631aca8e45b41f31b041b1fb
jiyoon <vywns9978@gmail.com>
2023-12-02 17:25:14 +0900

    setting: react-redux, redux-toolkit 설치

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import App from './App';
 import { GlobalStyles } from './style/global';
 import { defaultTheme } from './style/theme';
 import { ThemeProvider } from 'styled-components';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={defaultTheme}>
-			<GlobalStyles />
-			<App />
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider theme={defaultTheme}>
+				<GlobalStyles />
+				<App />
+			</ThemeProvider>
+		</Provider>
 	</React.StrictMode>,
 );
