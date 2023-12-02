d6f7b12cf3fb4a4610095fec78768f5ff6f93575
jiyoon <vywns9978@gmail.com>
2023-12-03 00:45:33 +0900

    style: SelectType 컴포넌트 생성 및 스타일 적용

---
+++ src/components/SelectType/index.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { FormControl, SelectChangeEvent } from '@mui/material';
+import { TypeSelect, TypeItem } from './style';
+
+export default function SelectType() {
+	const [questionType, setQuestionType] = useState('RadioType');
+
+	const handleTypeChange = (event: SelectChangeEvent) => {
+		setQuestionType(event.target.value);
+	};
+
+	return (
+		<FormControl sx={{ m: 1, minWidth: 180 }}>
+			<TypeSelect id="select-type" value={questionType} onChange={handleTypeChange}>
+				<TypeItem value="ShortType">단답형</TypeItem>
+				<TypeItem value="LongType">장문형</TypeItem>
+				<TypeItem value="RadioType" defaultChecked>
+					객관식 질문
+				</TypeItem>
+				<TypeItem value="CheckboxType">체크박스</TypeItem>
+				<TypeItem value="DndType">드롭다운</TypeItem>
+			</TypeSelect>
+		</FormControl>
+	);
+}
---
+++ src/components/SelectType/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import { MenuItem, Select } from '@mui/material';
+
+export const TypeSelect = styled(Select)`
+	font-size: 0.8125rem;
+	height: 45px;
+	padding-left: 5px;
+	line-height: 45px;
+`;
+
+export const TypeItem = styled(MenuItem)`
+	font-size: 0.8125rem;
+`;
