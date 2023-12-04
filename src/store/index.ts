81744d4db904511952763b8460f428bdf611d0fe
jiyoon <vywns9978@gmail.com>
2023-12-04 20:43:17 +0900

    fix: persist와의 충돌로 인한 rtk의 직렬화 검사 에러 해결

--- src/store/index.ts
+++ src/store/index.ts
@@ -19,6 +19,12 @@ const persistedReducer = persistReducer(persistConfg, rootReducer);
 
 export const store = configureStore({
 	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/REGISTER'],
+			},
+		}),
 });
 
 export const persistor = persistStore(store);
