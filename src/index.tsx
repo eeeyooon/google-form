abefb88236036913087bded4e8a1dcea05c3c0b8
jiyoon <vywns9978@gmail.com>
2023-12-04 20:40:47 +0900

    setting: redux-persist 설치 및 설정, 루트에도 persist 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,22 @@ import App from './App';
 import { GlobalStyles } from './style/global';
 import { defaultTheme } from './style/theme';
 import { ThemeProvider } from 'styled-components';
-import store from './store';
+import store, { persistor } from './store';
 import { Provider } from 'react-redux';
 import { StyledEngineProvider } from '@mui/styled-engine';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
 		<StyledEngineProvider injectFirst>
 			<Provider store={store}>
-				<ThemeProvider theme={defaultTheme}>
-					<GlobalStyles />
-					<App />
-				</ThemeProvider>
+				<PersistGate loading={null} persistor={persistor}>
+					<ThemeProvider theme={defaultTheme}>
+						<GlobalStyles />
+						<App />
+					</ThemeProvider>
+				</PersistGate>
 			</Provider>
 		</StyledEngineProvider>
 	</React.StrictMode>,
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { questionSlice } from './../slices/questionSlice';
 import { formSlice } from '../slices/formSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import { combineReducers } from 'redux';
 
-const store = configureStore({
-	reducer: {
-		form: formSlice.reducer,
-		question: questionSlice.reducer,
-	},
+const rootReducer = combineReducers({
+	form: formSlice.reducer,
+	question: questionSlice.reducer,
 });
+
+const persistConfg = {
+	key: 'root',
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfg, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
