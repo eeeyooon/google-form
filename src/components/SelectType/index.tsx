ff06a87fec9b7fe07cb98b5407ed924231b3c8dc
jiyoon <vywns9978@gmail.com>
2023-12-05 15:44:52 +0900

    feat: card 복사 시 selectedType도 기존 카드와 같게 렌더링하도록 수정

--- src/components/SelectType/index.tsx
+++ src/components/SelectType/index.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FormControl, SelectChangeEvent } from '@mui/material';
 import { TypeSelect, TypeItem } from './styles';
 import { useDispatch } from 'react-redux';
 import { setQuestionType } from '../../slices/questionSlice';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 type SelectTypeProps = {
 	cardId: number;
 };
 export default function SelectType({ cardId }: SelectTypeProps) {
 	const dispatch = useDispatch();
-	const [selectedType, setSelectedType] = useState('RadioType');
+	const currentType = useSelector((state: RootState) => state.question.cards[cardId]).questionType;
+	const [selectedType, setSelectedType] = useState(currentType);
+
+	useEffect(() => {
+		setSelectedType(currentType);
+	}, [currentType]);
 
 	const handleTypeChange = (event: SelectChangeEvent) => {
 		setSelectedType(event.target.value);
@@ -21,9 +28,7 @@ export default function SelectType({ cardId }: SelectTypeProps) {
 			<TypeSelect id="select-type" value={selectedType} onChange={handleTypeChange}>
 				<TypeItem value="ShortType">단답형</TypeItem>
 				<TypeItem value="LongType">장문형</TypeItem>
-				<TypeItem value="RadioType" defaultChecked>
-					객관식 질문
-				</TypeItem>
+				<TypeItem value="RadioType">객관식 질문</TypeItem>
 				<TypeItem value="CheckboxType">체크박스</TypeItem>
 				<TypeItem value="DndType">드롭다운</TypeItem>
 			</TypeSelect>
