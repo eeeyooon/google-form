6a1aee4c32bd585ec304c8cbcf19b5e1d764c8cf
jiyoon <vywns9978@gmail.com>
2023-12-05 20:05:44 +0900

    revert: react-dnd 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Form, Preview, Submit } from './pages';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<DndProvider backend={HTML5Backend}>
-					<Route path="/" element={<Form />} />
-				</DndProvider>
+				<Route path="/" element={<Form />} />
 				<Route path="/preview" element={<Preview />} />
 				<Route path="/submit" element={<Submit />} />
 			</Routes>
